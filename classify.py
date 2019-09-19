5b655d5b6c67f5f55864492001c47329dba65e2d
Clay Schubiner <clayschubiner@gmail.com>
2019-09-18 22:32:54 -0700

    successful df

---
+++ classify.py
@@ -0,0 +1,38 @@
+import os
+from image_features import image_features
+import numpy as np
+import pandas as pd
+
+
+front_images = ['images/front/' + str(x) for x in os.listdir('images/front')]
+back_images = ['images/back/' + str(x) for x in os.listdir('images/back')]
+
+front_x = zip(front_images, [True for x in front_images])
+back_x = zip(back_images, [False for x in back_images])
+d = list(front_x) + list(back_x)
+print(d)
+
+df = pd.DataFrame(data=d)
+print(df)
+df = df.rename(columns={0: "image_name", 1: "is_front"})
+print(df)
+
+
+# n = round(0.8 * len(img_paths))
+# n = round(0.8 * len(X_train))
+# X_train = image_features(img_paths[:n], progress=True)
+# y_train = no_layer[:n]
+
+# X_val = image_features(img_paths[n:], progress=True)
+# y_val = no_layer[n:]
+
+
+# from sklearn import linear_model
+# clf = linear_model.LogisticRegressionCV(
+#     max_iter=1000,
+#     Cs=np.geomspace(1e-1, 1e-7, 15),
+#     class_weight='balanced'
+# )
+# clf.fit(X_train, y_train)
+# print('train score:', clf.score(X_train, y_train))
+# print('val score:', clf.score(X_val, y_val))
