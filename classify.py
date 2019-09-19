fcbcdef4268c8d70c9b3ae6d3b5e3ae7295d4156
Clay Schubiner <clayschubiner@gmail.com>
2019-09-18 22:56:08 -0700

    zip 2

--- classify.py
+++ classify.py
@@ -1,18 +1,24 @@
+import functools
 import os
 from image_features import image_features
 import numpy as np
 import pandas as pd
 
-
+training_directories = ['front', 'back']
+file_names = [(dir_name, ['images/' + dir_name + '/' + str(x) for x in os.listdir('images/' + dir_name)]) for dir_name in training_directories]
 front_images = ['images/front/' + str(x) for x in os.listdir('images/front')]
 back_images = ['images/back/' + str(x) for x in os.listdir('images/back')]
+
+file_names_zipped_with_label = [list(zip(front_images, [dir_name for x in front_images])) for dir_name, front_images in file_names]
+ret = functools.reduce(lambda x,y: x+y, file_names_zipped_with_label)
+print(ret)
 
 front_x = zip(front_images, [True for x in front_images])
 back_x = zip(back_images, [False for x in back_images])
 d = list(front_x) + list(back_x)
-print(d)
+# print(d)
 
-df = pd.DataFrame(data=d)
+df = pd.DataFrame(data=ret)
 print(df)
 df = df.rename(columns={0: "image_name", 1: "is_front"})
 print(df)
