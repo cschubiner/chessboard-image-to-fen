35175abd9b53701849c38b35fef7766da9714d41
Clay Schubiner <clayschubiner@gmail.com>
2019-09-18 23:35:52 -0700

    ignore images

--- classify.py
+++ classify.py
@@ -5,33 +5,25 @@ import numpy as np
 import pandas as pd
 
 training_directories = ['front', 'back', '7']
-file_names = [(dir_name, ['images/' + dir_name + '/' + str(x) for x in os.listdir('images/' + dir_name)]) for dir_name in training_directories]
-front_images = ['images/front/' + str(x) for x in os.listdir('images/front')]
-back_images = ['images/back/' + str(x) for x in os.listdir('images/back')]
-
+training_directories = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13']
+file_names = [(dir_name, ['images/' + dir_name + '/' + str(x) for x in os.listdir('images/' + dir_name) if x.endswith('.jpg')]) for dir_name in training_directories]
 file_names_zipped_with_label = [list(zip(front_images, [dir_name for x in front_images])) for dir_name, front_images in file_names]
-ret = functools.reduce(lambda x,y: x+y, file_names_zipped_with_label)
-print(ret)
 
-front_x = zip(front_images, [True for x in front_images])
-back_x = zip(back_images, [False for x in back_images])
-d = list(front_x) + list(back_x)
-# print(d)
-
-df = pd.DataFrame(data=ret)
-df = df.rename(columns={0: "image_name", 1: "is_front"})
+df = pd.DataFrame(data=functools.reduce(lambda x,y: x+y, file_names_zipped_with_label))
+df = df.rename(columns={0: "image_name", 1: "label"})
 df = df.sample(frac=1).reset_index(drop=True) # shuffle rows
 print(df)
 
 img_paths = list(df['image_name'])
-is_front = list(df['is_front'])
+label = list(df['label'])
 
 n = round(0.8 * len(img_paths))
+# n = round(0.1 * len(img_paths))
 X_train = image_features(img_paths[:n], progress=True)
-y_train = is_front[:n]
+y_train = label[:n]
 
 X_val = image_features(img_paths[n:], progress=True)
-y_val = is_front[n:]
+y_val = label[n:]
 
 
 from sklearn import linear_model
