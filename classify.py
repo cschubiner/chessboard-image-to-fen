00f395b1a9beb132ea0040036a42c9b5812440da
Clay Schubiner <clayschubiner@gmail.com>
2019-09-18 22:46:07 -0700

    cat dog

--- classify.py
+++ classify.py
@@ -18,21 +18,23 @@ df = df.rename(columns={0: "image_name", 1: "is_front"})
 print(df)
 
 
-# n = round(0.8 * len(img_paths))
-# n = round(0.8 * len(X_train))
-# X_train = image_features(img_paths[:n], progress=True)
-# y_train = no_layer[:n]
+img_paths = list(df['image_name'])
+is_front = list(df['is_front'])
 
-# X_val = image_features(img_paths[n:], progress=True)
-# y_val = no_layer[n:]
+n = round(0.8 * len(img_paths))
+X_train = image_features(img_paths[:n], progress=True)
+y_train = is_front[:n]
+
+X_val = image_features(img_paths[n:], progress=True)
+y_val = is_front[n:]
 
 
-# from sklearn import linear_model
-# clf = linear_model.LogisticRegressionCV(
-#     max_iter=1000,
-#     Cs=np.geomspace(1e-1, 1e-7, 15),
-#     class_weight='balanced'
-# )
-# clf.fit(X_train, y_train)
-# print('train score:', clf.score(X_train, y_train))
-# print('val score:', clf.score(X_val, y_val))
+from sklearn import linear_model
+clf = linear_model.LogisticRegressionCV(
+    max_iter=1000,
+    Cs=np.geomspace(1e-1, 1e-7, 15),
+    class_weight='balanced'
+)
+clf.fit(X_train, y_train)
+print('train score:', clf.score(X_train, y_train))
+print('val score:', clf.score(X_val, y_val))
