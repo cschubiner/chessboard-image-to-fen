e4c27ddfac05ce85ce59a7e51bedd35d11ea7681
Clay Schubiner <clayschubiner@gmail.com>
2019-09-21 16:57:14 -0700

    it works

---
+++ eval_classify.py
@@ -0,0 +1,39 @@
+import functools
+import os
+import numpy as np
+import pandas as pd
+from utils import split_board_image
+
+board_image_dir = 'to_eval'
+board_file_names = [str(x) for x in os.listdir(board_image_dir)]
+
+pieces_image_dir = 'to_eval_pieces'
+
+for fname in board_file_names:
+  split_board_image(board_image_dir + '/' + fname, fname, pieces_image_dir)
+
+
+from image_features import image_features
+
+pieces_file_names = [str(x) for x in os.listdir(pieces_image_dir)]
+pieces_file_locs = [pieces_image_dir + '/' + x for x in pieces_file_names]
+pieces_image_features = image_features(pieces_file_locs)
+
+clf = load('clf.joblib')
+pieces_preds = clf.predict(pieces_image_features)
+
+print(len(pieces_file_names), len(pieces_file_locs), len(pieces_image_features), len(pieces_preds))
+assert len(pieces_file_names) == len(pieces_file_locs) == len(pieces_image_features) == len(pieces_preds)
+assert 64 == len(pieces_file_names)
+
+pred_board_dict = dict()
+for index, fname in enumerate(pieces_file_names):
+  i, j, key = fname[0], fname[2], fname[:3]
+  pred = pieces_preds[index]
+  pred_board_dict[key] = pred
+
+for i in range(8):
+  for j in range(8):
+    key = f"{i}_{j}"
+    print(pred_board_dict[key], end = ' ')
+  print('')
---
+++ utils.py
@@ -0,0 +1,17 @@
+import cv2
+
+
+def split_board_image(floc, fname, out_dir, board=None):
+  img = cv2.imread(floc)
+  square_size = 150
+  for r in range(0, img.shape[0], 150):
+    i = r // square_size
+    for c in range(0, img.shape[1], 150):
+      j = c // square_size
+      if board:
+        piece = board[i][j]
+        out_loc = f"{out_dir}/{piece}/_{fname}_{r}_{c}.jpg"
+      else:
+        out_loc = f"{out_dir}/{i}_{j}.jpg"
+
+      cv2.imwrite(out_loc, img[r:r + square_size, c:c + square_size, :])
