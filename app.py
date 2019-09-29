07a754466d3454ef9630ec8a60e507da52836945
Clay Schubiner <clayschubiner@gmail.com>
2019-09-29 00:31:52 -0700

    more

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask
 
+from eval_classify import run
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/chess')
 def index():
-    return 'Whale, Hello there!'
+    result = run()
+    board_s = '\n'.join(result)
+    return 'Whale, Hello there!3\n' + board_s
 
 @app.route('/')
 def hello_whale():
-    return 'Whale, Hello there!'
+    return 'Whale, Hello there!1'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
