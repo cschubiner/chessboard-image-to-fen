cf4eeae54ac5f49b48a96d5497206aad9709e934
Clay Schubiner <clayschubiner@gmail.com>
2019-09-29 00:29:29 -0700

    more

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # chessboard-image-to-fen
 Takes an overhead picture of a chessboard and converts it to FEN notation for engine analysis
 
+# Running server
+psudo() { sudo env PATH="$PATH" "$@"; }
+
+psudo python3 app.py
+
 ## Notes
 
 If you get this error with image_features, follow steps here:
--- eval_classify.py
+++ eval_classify.py
@@ -13,28 +13,37 @@ pieces_image_dir = 'to_eval_pieces'
 
 clf = load('clf.joblib')
 
-for fname in board_file_names:
-  print("Evaluating " + str(fname) + "...")
-  split_board_image(board_image_dir + '/' + fname, fname, pieces_image_dir)
+def run():
+  for fname in board_file_names:
+    print("Evaluating " + str(fname) + "...")
+    split_board_image(board_image_dir + '/' + fname, fname, pieces_image_dir)
 
-  pieces_file_names = [str(x) for x in os.listdir(pieces_image_dir)]
-  pieces_file_locs = [pieces_image_dir + '/' + x for x in pieces_file_names]
-  pieces_image_features = image_features(pieces_file_locs)
+    pieces_file_names = [str(x) for x in os.listdir(pieces_image_dir)]
+    pieces_file_locs = [pieces_image_dir + '/' + x for x in pieces_file_names]
+    pieces_image_features = image_features(pieces_file_locs)
 
-  pieces_preds = clf.predict(pieces_image_features)
+    pieces_preds = clf.predict(pieces_image_features)
 
-  print(len(pieces_file_names), len(pieces_file_locs), len(pieces_image_features), len(pieces_preds))
-  assert len(pieces_file_names) == len(pieces_file_locs) == len(pieces_image_features) == len(pieces_preds)
-  assert 64 == len(pieces_file_names)
+    print(len(pieces_file_names), len(pieces_file_locs), len(pieces_image_features), len(pieces_preds))
+    assert len(pieces_file_names) == len(pieces_file_locs) == len(pieces_image_features) == len(pieces_preds)
+    assert 64 == len(pieces_file_names)
 
-  pred_board_dict = dict()
-  for index, fname in enumerate(pieces_file_names):
-    i, j, key = fname[0], fname[2], fname[:3]
-    pred = pieces_preds[index]
-    pred_board_dict[key] = pred
+    pred_board_dict = dict()
+    for index, fname in enumerate(pieces_file_names):
+      i, j, key = fname[0], fname[2], fname[:3]
+      pred = pieces_preds[index]
+      pred_board_dict[key] = pred
 
-  for i in range(8):
-    for j in range(8):
-      key = str(i) + "_" + str(j)
-      print(pred_board_dict[key], end = ' ')
-    print('')
+    ret = list()
+    for i in range(8):
+      row = ''
+      for j in range(8):
+        key = str(i) + "_" + str(j)
+        row += pred_board_dict[key] + ' '
+      ret.append(row.strip())
+    return ret
+
+if __name__ == "__main__":
+  result = run()
+  print('\n'.join(result))
+
