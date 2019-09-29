5f52810e6ba40fc3ff998861079ac67d34528b3e
Clay Schubiner <clayschubiner@gmail.com>
2019-09-29 00:40:02 -0700

    image uploads

--- app.py
+++ app.py
@@ -1,8 +1,37 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+import os
+
+from flask import Flask, request
+
 
 from eval_classify import run
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 
 app = Flask(__name__)
+app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '/uploads'
+
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+patch_request_class(app)  # set maximum file size, default is 16MB
+
+html = '''
+    <!DOCTYPE html>
+    <title>Upload File</title>
+    <h1>Photo Upload</h1>
+    <form method=post enctype=multipart/form-data>
+         <input type=file name=photo>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST' and 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        file_url = photos.url(filename)
+        return html + '<br><img src=' + file_url + '>'
+    return html
 
 
 @app.route('/chess')
