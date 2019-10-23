1b917d0766f7c24f4191b1602a6ba5689e4c3a46
Clay Schubiner <clayschubiner@gmail.com>
2019-10-23 09:59:21 -0700

    more

--- learn_classify.py
+++ learn_classify.py
@@ -56,6 +56,7 @@ img_paths = list(df['image_name'])
 label = list(df['label'])
 
 def get_with_hash(obj_to_hash, cache_miss_function):
+  # return cache_miss_function()
   key = str(obj_to_hash)[:8]
   filename = 'saved_objects/obj_' + key
   if os.path.exists(filename):
@@ -65,7 +66,7 @@ def get_with_hash(obj_to_hash, cache_miss_function):
   print('Could not find', filename, 'so manually calculating it...')
   ret = cache_miss_function()
   with open(filename, 'wb') as f:
-    pickle.dump(ret, f, pickle.HIGHEST_PROTOCOL)
+    pickle.dump(ret, f)
   return ret
 
 
@@ -85,47 +86,66 @@ y_val = y_full[n:]
 from sklearn import linear_model, ensemble
 
 def validate_score_clf(clf, name):
+  global best_val_score
+  global best_clf
   print('Fitting', name)
   try:
     clf.fit(X_train, y_train)
     print(name, '.8 - train score:', clf.score(X_train, y_train))
-    print(name, '.8 - val score:', clf.score(X_val, y_val))
+    val_score = clf.score(X_val, y_val)
+    print(name, '.8 - val score:', val_score)
+    if val_score > best_val_score:
+      print('New best val score!!')
+      best_clf = clf
   except Exception as e:
     print("Exception!", e)
     try:
       clf.fit_transform(X_train, y_train)
       print(name, '.8 - train score:', clf.score(X_train, y_train))
-      print(name, '.8 - val score:', clf.score(X_val, y_val))
+      val_score = clf.score(X_val, y_val)
+      print(name, '.8 - val score:', val_score)
+      if val_score > best_val_score:
+        print('New best val score!!')
+        best_clf = clf
     except Exception as e:
       print("Exception 2!", e)
 
-# validate_score_clf(linear_model.LarsCV(max_iter=1200), 'linear_model.LarsCV')
-# validate_score_clf(linear_model.LassoLarsCV(max_iter=1200), 'linear_model.LassoLarsCV')
-# validate_score_clf(linear_model.LassoCV(max_iter=1200), 'linear_model.LassoCV')
-# validate_score_clf(linear_model.ElasticNetCV(max_iter=1200), 'linear_model.ElasticNetCV')
-# validate_score_clf(linear_model.OrthogonalMatchingPursuitCV(), 'linear_model.OrthogonalMatchingPursuitCV')
-# validate_score_clf(ensemble.GradientBoostingClassifier(n_estimators=10, verbose=1), 'GradientBoostingClassifier')
-# validate_score_clf(linear_model.RidgeClassifierCV(), 'linear_model.RidgeClassifierCV')
+best_clf = None
+best_val_score = 0.95
+validate_score_clf(linear_model.PassiveAggressiveClassifier(max_iter=1200), 'linear_model.PassiveAggressiveClassifier')
+validate_score_clf(linear_model.SGDClassifier(max_iter=1200), 'linear_model.SGDClassifier')
+validate_score_clf(linear_model.LarsCV(max_iter=1200), 'linear_model.LarsCV')
+validate_score_clf(linear_model.LassoLarsCV(max_iter=1200), 'linear_model.LassoLarsCV')
+validate_score_clf(linear_model.LassoCV(max_iter=1200), 'linear_model.LassoCV')
+validate_score_clf(linear_model.ElasticNetCV(max_iter=1200), 'linear_model.ElasticNetCV')
+validate_score_clf(linear_model.OrthogonalMatchingPursuitCV(), 'linear_model.OrthogonalMatchingPursuitCV')
+validate_score_clf(ensemble.GradientBoostingClassifier(n_estimators=15, verbose=1), 'GradientBoostingClassifier')
+validate_score_clf(linear_model.RidgeClassifierCV(), 'linear_model.RidgeClassifierCV')
 
-# validate_score_clf(linear_model.ElasticNetCV(max_iter=1200), 'linear_model.ElasticNetCV')
-# validate_score_clf(ensemble.RandomForestClassifier(verbose=1), 'RandomForestClassifier')
+validate_score_clf(ensemble.RandomForestClassifier(verbose=1), 'RandomForestClassifier')
 
 clf = linear_model.LogisticRegressionCV(
-    max_iter=1200, Cs=np.geomspace(1e-1, 1e-7, 15), class_weight='balanced', verbose=1
+    max_iter=2000, Cs=np.geomspace(1e-1, 1e-7, 15), class_weight='balanced', verbose=1
 )
-validate_score_clf(clf, 'LogisticRegressionCV')
+validate_score_clf(clf, 'LogisticRegressionCV maxiter2000')
 
+clf = linear_model.LogisticRegressionCV(
+    max_iter=900, Cs=np.geomspace(1e-1, 1e-7, 15), class_weight='balanced', verbose=1
+)
+validate_score_clf(clf, 'LogisticRegressionCV maxiter900')
 
-clf.fit(X_full, y_full)
-dump(clf, 'clf.joblib')
+if best_clf:
+  print('Fitting best_clf with val score:', best_val_score)
+  best_clf.fit(X_full, y_full)
+  dump(best_clf, 'clf.joblib')
 
 print('full train score:', clf.score(X_train, y_train))
 print('full val score:', clf.score(X_val, y_val))
 print('full full score:', clf.score(X_full, y_full))
 
 from tpot import TPOTClassifier
-# tpot_classifier = TPOTClassifier(generations=5, population_size=20, cv=5, random_state=42, verbosity=2)
-tpot_classifier = TPOTClassifier(generations=9, population_size=20, config_dict='TPOT light', verbosity=2)
+tpot_classifier = TPOTClassifier(generations=5, population_size=20, cv=5, random_state=42, verbosity=2)
+# tpot_classifier = TPOTClassifier(generations=9, population_size=20, config_dict='TPOT light', verbosity=2)
 validate_score_clf(tpot_classifier, 'TPOTClassifier')
 tpot_classifier.export('tpot_exported_pipeline.py')
 
--- tpot_exported_pipeline.py
+++ tpot_exported_pipeline.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import Binarizer
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +11,11 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=None)
 
-# Average CV score on the training set was:0.3836625316738334
-exported_pipeline = LogisticRegression(C=0.01, dual=False, penalty="l1")
+# Average CV score on the training set was:0.9360402519866234
+exported_pipeline = make_pipeline(
+    Binarizer(threshold=0.35000000000000003),
+    LogisticRegression(C=0.1, dual=False, penalty="l2")
+)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
